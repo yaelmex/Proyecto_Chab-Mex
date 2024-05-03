b7f2d08371e66d754882ef96a5766b72dea76055
Jose Yael <160161055+yaelmex@users.noreply.github.com>
2024-05-03 13:22:05 -0600

    Add files via upload

---
+++ bd_inversiones/bd_inversiones_operaciones.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bd_inversiones
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `operaciones`
+--
+
+DROP TABLE IF EXISTS `operaciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `operaciones` (
+  `NumOperacion` int NOT NULL AUTO_INCREMENT,
+  `Operacion` varchar(45) NOT NULL,
+  `Monto` double NOT NULL,
+  `Fecha` varchar(45) NOT NULL,
+  PRIMARY KEY (`NumOperacion`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `operaciones`
+--
+
+LOCK TABLES `operaciones` WRITE;
+/*!40000 ALTER TABLE `operaciones` DISABLE KEYS */;
+INSERT INTO `operaciones` VALUES (1,'hola',200,'hola'),(2,'Inversión',100,'2024-05-01 - 2024-08-01'),(3,'Inversión',100,'2024-05-02 - 2024-11-02'),(4,'Inversión',2000,'2024-05-02 - 2024-08-02'),(5,'Inversión',1000,'2024-05-02 - 2025-05-02'),(6,'Inversión en: CETES DIRECTO',100,'2024-05-03 - 2024-08-03');
+/*!40000 ALTER TABLE `operaciones` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-03 13:15:49
